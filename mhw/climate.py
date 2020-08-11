c367d47ac40df689c6a4a541cdb00787e4b65be4
profxj <xavier@ucolick.org>
2020-08-11 12:54:19 -0700

    full climate

--- mhw/climate.py
+++ mhw/climate.py
@@ -1,15 +1,186 @@
 """ Climate calculations.
 """
 
+import os
+import glob
 
 import numpy as np
 from datetime import date
 
-from mhw import utils
+import iris
+
+from mhw import utils as mhw_utils
+from mhw import numba as mhw_numba
 
 from IPython import embed
 
+
+def noaa_seas_thresh(climate_db_file,
+               noaa_path='/home/xavier/Projects/Oceanography/data/SST/NOAA-OI-SST-V2/',
+               climatologyPeriod=(1983, 2012), cut_sky=True, all_sst=None,
+               min_frac=0.9, n_calc=None, debug=False):
+    """
+    Build climate model for NOAA OI data
+
+    Parameters
+    ----------
+    climate_db_file : str, output filename.  Should have extension .nc
+    noaa_path
+    climatologyPeriod
+    cut_sky
+    all_sst
+    min_frac
+    n_calc
+    debug
+
+    Returns
+    -------
+
+    """
+    # Grab the list of SST V2 files
+    all_sst_files = glob.glob(noaa_path + 'sst*nc')
+    all_sst_files.sort()
+    # Cut on years
+    if '1981' not in all_sst_files[0]:
+        raise ValueError("Years not in sync!!")
+
+    # Load the Cubes into memory
+    if all_sst is None:
+        istart = climatologyPeriod[0] - 1981
+        iend = climatologyPeriod[1] - 1981 + 1
+        all_sst_files = all_sst_files[istart:iend]
+
+        print("Loading up the files. Be patient...")
+        all_sst = mhw_utils.load_noaa_sst(all_sst_files)
+
+    # Coords
+    lat_coord = all_sst[0].coord('latitude')
+    lon_coord = all_sst[0].coord('longitude')
+
+    # Time
+    t = mhw_utils.grab_t(all_sst)
+    time_dict = build_time_dict(t)
+
+    # Start the db's
+    if os.path.isfile(climate_db_file):
+        os.remove(climate_db_file)
+
+    # Main loop
+    if cut_sky:
+        irange = np.arange(355, 365)
+        jrange = np.arange(715,725)
+    else:
+        irange = np.arange(lat_coord.shape[0])
+        jrange = np.arange(lon_coord.shape[0])
+    ii_grid, jj_grid = np.meshgrid(irange, jrange)
+    ii_grid = ii_grid.flatten()
+    jj_grid = jj_grid.flatten()
+    if n_calc is None:
+        n_calc = len(irange) * len(jrange)
+
+    # Init
+    lenClimYear = 366  # This has to match what is in climate.py
+    out_seas = np.zeros((lenClimYear, lat_coord.shape[0], lon_coord.shape[0]), dtype='float32')
+    out_thresh = np.zeros((lenClimYear, lat_coord.shape[0], lon_coord.shape[0]), dtype='float32')
+
+    counter = 0
+    tot_events = 0
+
+    # Init climate items
+
+    # Length of climatological year
+    lenClimYear = 366
+    feb29 = 60
+    # Window
+    windowHalfWidth=5
+    wHW_array = np.outer(np.ones(1000, dtype='int'), np.arange(-windowHalfWidth, windowHalfWidth + 1))
+
+    # Inialize arrays
+    thresh_climYear = np.NaN * np.zeros(lenClimYear, dtype='float32')
+    seas_climYear = np.NaN * np.zeros(lenClimYear, dtype='float32')
+
+    doyClim = time_dict['doy']
+    TClim = len(doyClim)
+
+    clim_start = 0
+    clim_end = len(doyClim)
+    nwHW = wHW_array.shape[1]
+
+    # Smoothing
+    smoothPercentile = True
+    smoothPercentileWidth = 31
+    pctile = 90
+
+    # Main loop
+    while (counter < n_calc):
+        # Init
+        thresh_climYear[:] = np.nan
+        seas_climYear[:] = np.nan
+
+        ilat = ii_grid[counter]
+        jlon = jj_grid[counter]
+        counter += 1
+        #
+        SST = mhw_utils.grab_T(all_sst, ilat, jlon)
+        frac = np.sum(np.invert(SST.mask))/t.size
+        if SST.mask is np.bool_(False) or frac > min_frac:
+            pass
+        else:
+            continue
+        # Work it
+        mhw_numba.calc_clim(lenClimYear, feb29, doyClim, clim_start, clim_end, wHW_array, nwHW,
+                     TClim, thresh_climYear, SST, pctile, seas_climYear)
+        thresh_climYear[feb29 - 1] = 0.5 * thresh_climYear[feb29 - 2] + 0.5 * thresh_climYear[feb29]
+        seas_climYear[feb29 - 1] = 0.5 * seas_climYear[feb29 - 2] + 0.5 * seas_climYear[feb29]
+
+
+        # Smooth if desired
+        if smoothPercentile:
+            thresh_climYear = mhw_utils.runavg(thresh_climYear, smoothPercentileWidth)
+            seas_climYear = mhw_utils.runavg(seas_climYear, smoothPercentileWidth)
+
+        # Save
+        out_seas[:, ilat, jlon] = seas_climYear
+        out_thresh[:, ilat, jlon] = thresh_climYear
+
+        # Count
+        print('count={} of {}.'.format(counter, n_calc))
+
+        # Cubes
+        if (counter % 100000 == 0) or (counter == n_calc):
+            print("Saving...")
+            cubes = iris.cube.CubeList()
+            time_coord = iris.coords.DimCoord(np.arange(lenClimYear), units='day', var_name='day')
+            cube_seas = iris.cube.Cube(out_seas, units='degC', var_name='seasonalT',
+                                       dim_coords_and_dims=[(time_coord, 0),
+                                                            (lat_coord, 1),
+                                                            (lon_coord, 2)])
+            cube_thresh = iris.cube.Cube(out_thresh, units='degC', var_name='threshT',
+                                         dim_coords_and_dims=[(time_coord, 0),
+                                                              (lat_coord, 1),
+                                                              (lon_coord, 2)])
+            cubes.append(cube_seas)
+            cubes.append(cube_thresh)
+            # Write
+            iris.save(cubes, climate_db_file, zlib=True)
+
+    print("All done!!")
+
+
+
 def build_time_dict(t):
+    """
+    Generate a time dict for guiding climate analysis
+
+    Parameters
+    ----------
+    t
+
+    Returns
+    -------
+    times : dict
+
+    """
 
     # Generate vectors for year, month, day-of-month, and day-of-year
     T = len(t)
@@ -43,156 +214,19 @@ def build_time_dict(t):
 
     return times
 
-def calc(times, temp, windowHalfWidth=5, maxPadLength=False, Ly=False,
-         smoothPercentile=True, smoothPercentileWidth=31, pctile=90,
-         parallel=False):
-    '''
-    Calculates climate
 
-    Inputs:
+# Command line execution
+if __name__ == '__main__':
 
-      times   Time vector, in datetime format (e.g., date(1982,1,1).toordinal())
-              [1D numpy array of length T of type int]
-      temp    Temperature vector [1D numpy array of length T]
+    # Traditional Climate
+    if False:
+        noaa_seas_thresh('/home/xavier/Projects/Oceanography/MHWs/db/NOAA_OI_climate_1983-2012.nc',
+                         climatologyPeriod=(1983, 2012),
+                         cut_sky=False)
 
-    Outputs:
-
-      clim    Climatology of SST. Each key (following list) is a seasonally-varying
-              time series [1D numpy array of length T] of a particular measure:
-
-        'thresh'               Seasonally varying threshold (e.g., 90th percentile)
-        'seas'                 Climatological seasonal cycle
-        'missing'              A vector of TRUE/FALSE indicating which elements in 
-                               temp were missing values for the MHWs detection
-
-    Options:
-
-      pctile                 Threshold percentile (%) for detection of extreme values
-                             (DEFAULT = 90)
-      smoothPercentile       Boolean switch indicating whether to smooth the threshold
-                             percentile timeseries with a moving average (DEFAULT = True)
-      smoothPercentileWidth  Width of moving average window for smoothing threshold
-                             (DEFAULT = 31 [days])
-      windowHalfWidth        Width of window (one sided) about day-of-year used for
-                             the pooling of values and calculation of threshold percentile
-                             (DEFAULT = 5 [days])
-      maxPadLength           Specifies the maximum length [days] over which to interpolate
-                             (pad) missing data (specified as nans) in input temp time series.
-                             i.e., any consecutive blocks of NaNs with length greater
-                             than maxPadLength will be left as NaN. Set as an integer.
-                             (DEFAULT = False, interpolates over all missing values).
-      Ly                     Specifies if the length of the year is < 365/366 days (e.g. a
-                             360 day year from a climate model). This affects the calculation
-                             of the climatology. (DEFAULT = False)
-
-    Notes:
-
-      1. This function assumes that the input time series consist of continuous daily values
-         with few missing values. Time ranges which start and end part-way through the calendar
-         year are supported.
-
-      2. This function supports leap years. This is done by ignoring Feb 29s for the initial
-         calculation of the climatology and threshold. The value of these for Feb 29 is then
-         linearly interpolated from the values for Feb 28 and Mar 1.
-
-      3. The calculation of onset and decline rates assumes that the heat wave started a half-day
-         before the start day and ended a half-day after the end-day. (This is consistent with the
-         duration definition as implemented, which assumes duration = end day - start day + 1.)
-
-      4. For the purposes of MHW detection, any missing temp values not interpolated over (through
-         optional maxPadLLength) will be set equal to the seasonal climatology. This means they will
-         trigger the end/start of any adjacent temp values which satisfy the MHW criteria.
-
-      5. If the code is used to detect cold events (coldSpells = True), then it works just as for heat
-         waves except that events are detected as deviations below the (100 - pctile)th percentile
-         (e.g., the 10th instead of 90th) for at least 5 days. Intensities are reported as negative
-         values and represent the temperature anomaly below climatology.
-
-    Written by Eric Oliver, Institue for Marine and Antarctic Studies, University of Tasmania, Feb 2015
-
-    '''
-    import pdb; pdb.set_trace()  #  I BROKE THIS UP AND USE MY OWN HOME-BREW in mhw_analysis in db/
-
-    #
-    # Time and dates vectors
-    #
-
-    # Constants (doy values for Feb-28 and Feb-29) for handling leap-years
-    feb29 = 60
-
-    #
-    # Calculate threshold and seasonal climatology (varying with day-of-year)
-    #
-    tempClim = temp
-    doyClim = times['doy']
-    TClim = len(doyClim)
-
-    # Pad missing values for all consecutive missing blocks of length <= maxPadLength
-    if maxPadLength:
-        temp = utils.pad(temp, maxPadLength=maxPadLength)
-        tempClim = utils.pad(tempClim, maxPadLength=maxPadLength)
-
-    # Length of climatological year
-    lenClimYear = 366
-    # Start and end indices
-    #clim_start = np.where(yearClim == climatologyPeriod[0])[0][0]
-    #clim_end = np.where(yearClim == climatologyPeriod[1])[0][-1]
-    clim_start = 0
-    clim_end = len(doyClim)
-
-    # Inialize arrays
-    thresh_climYear = np.NaN*np.zeros(lenClimYear, dtype='float32')
-    seas_climYear = np.NaN*np.zeros(lenClimYear, dtype='float32')
-    clim = {}
-    #clim['thresh'] = np.NaN*np.zeros(TClim)
-    #clim['seas'] = np.NaN*np.zeros(TClim)
-    wHW_array = np.outer(np.ones(1000, dtype='int'), np.arange(-windowHalfWidth, windowHalfWidth+1))
-    nwHW = wHW_array.shape[1]
-
-    # Loop over all day-of-year values, and calculate threshold and seasonal climatology across years
-    if parallel:
-        doit(lenClimYear, feb29, doyClim, clim_start, clim_end, wHW_array, nwHW,
-                 TClim, thresh_climYear, tempClim, pctile, seas_climYear)
-    else:
-        for d in range(1,lenClimYear+1):
-            # Special case for Feb 29
-            if d == feb29:
-                continue
-            # find all indices for each day of the year +/- windowHalfWidth and from them calculate the threshold
-            tt0 = np.where(doyClim[clim_start:clim_end+1] == d)[0]
-            # If this doy value does not exist (i.e. in 360-day calendars) then skip it
-            if len(tt0) == 0:
-                continue
-            #tt = np.array([])
-            #for w in range(-windowHalfWidth, windowHalfWidth+1):
-            #    tt = np.append(tt, clim_start+tt0 + w)
-            tt = (wHW_array[0:len(tt0),:] + np.outer(tt0, np.ones(nwHW, dtype='int'))).flatten()
-            gd = np.all([tt >= 0, tt<TClim], axis=0)
-            tt = tt[gd] # Reject indices "after" the last element
-            thresh_climYear[d-1] = np.nanpercentile(tempClim[tt], pctile)
-            seas_climYear[d-1] = np.nanmean(tempClim[tt])
-    # Special case for Feb 29
-    thresh_climYear[feb29-1] = 0.5*thresh_climYear[feb29-2] + 0.5*thresh_climYear[feb29]
-    seas_climYear[feb29-1] = 0.5*seas_climYear[feb29-2] + 0.5*seas_climYear[feb29]
-
-    # Smooth if desired
-    if smoothPercentile:
-        # If the length of year is < 365/366 (e.g. a 360 day year from a Climate Model)
-        if Ly:
-            valid = ~np.isnan(thresh_climYear)
-            thresh_climYear[valid] = utils.runavg(thresh_climYear[valid], smoothPercentileWidth)
-            valid = ~np.isnan(seas_climYear)
-            seas_climYear[valid] = utils.runavg(seas_climYear[valid], smoothPercentileWidth)
-        # >= 365-day year
-        else:
-            thresh_climYear = utils.runavg(thresh_climYear, smoothPercentileWidth)
-            seas_climYear = utils.runavg(seas_climYear, smoothPercentileWidth)
-
-    # Generate threshold for full time series
-    clim['thresh'] = thresh_climYear.astype('float32')#[doyClim-1]
-    clim['seas'] = seas_climYear.astype('float32')#[doyClim-1]
-    # Save vector indicating which points in temp are missing values
-    #clim['missing'] = np.isnan(temp)
-
-    return clim
+    # Full Climate 1983-2019
+    if True:
+        noaa_seas_thresh('/home/xavier/Projects/Oceanography/MHW/db/NOAA_OI_climate_1983-2019.nc',
+                         climatologyPeriod=(1983, 2019),
+                         cut_sky=False)
 
