ea51840c988dd657bdea9d97e3d1bf6bb40f49d3
profxj <xavier@ucolick.org>
2020-08-12 11:50:36 -0700

    further progress

--- mhw/climate.py
+++ mhw/climate.py
@@ -3,10 +3,15 @@
 
 import os
 import glob
+from pkg_resources import resource_filename
 
 import numpy as np
-from datetime import date
+from scipy import signal
 
+from datetime import date
+import datetime
+
+import pandas
 import iris
 
 from mhw import utils as mhw_utils
@@ -16,16 +21,20 @@ from IPython import embed
 
 
 def noaa_seas_thresh(climate_db_file,
-               noaa_path='/home/xavier/Projects/Oceanography/data/SST/NOAA-OI-SST-V2/',
-               climatologyPeriod=(1983, 2012), cut_sky=True, all_sst=None,
-               min_frac=0.9, n_calc=None, debug=False):
+                     noaa_path=None,
+                     climatologyPeriod=(1983, 2012),
+                     cut_sky=True, all_sst=None,
+                     scale_file=None,
+                     min_frac=0.9, n_calc=None, debug=False):
     """
     Build climate model for NOAA OI data
 
     Parameters
     ----------
-    climate_db_file : str, output filename.  Should have extension .nc
-    noaa_path
+    climate_db_file : str
+        output filename.  Should have extension .nc
+    noaa_path : str, optional
+        Path to NOAA OI SST files
     climatologyPeriod
     cut_sky
     all_sst
@@ -37,6 +46,10 @@ def noaa_seas_thresh(climate_db_file,
     -------
 
     """
+    # Path
+    if noaa_path is None:
+        noaa_path = os.getenv("NOAA_OI")
+
     # Grab the list of SST V2 files
     all_sst_files = glob.glob(noaa_path + 'sst*nc')
     all_sst_files.sort()
@@ -60,6 +73,15 @@ def noaa_seas_thresh(climate_db_file,
     # Time
     t = mhw_utils.grab_t(all_sst)
     time_dict = build_time_dict(t)
+
+    scls = np.zeros_like(t).astype(float)
+    if scale_file is not None:
+        # Use scales
+        scale_tbl = pandas.read_hdf(scale_file, 'median_climate')
+        for kk, it in enumerate(t):
+            mtch = np.where(scale_tbl.index.to_pydatetime() == datetime.datetime.fromordinal(it))[0][0]
+            scls[kk] = scale_tbl.medSSTa_savgol[mtch]
+
 
     # Start the db's
     if os.path.isfile(climate_db_file):
@@ -120,14 +142,17 @@ def noaa_seas_thresh(climate_db_file,
         ilat = ii_grid[counter]
         jlon = jj_grid[counter]
         counter += 1
-        #
+
+        # Grab SST values
         SST = mhw_utils.grab_T(all_sst, ilat, jlon)
         frac = np.sum(np.invert(SST.mask))/t.size
         if SST.mask is np.bool_(False) or frac > min_frac:
             pass
         else:
             continue
+
         # Work it
+        SST -= scls
         mhw_numba.calc_clim(lenClimYear, feb29, doyClim, clim_start, clim_end, wHW_array, nwHW,
                      TClim, thresh_climYear, SST, pctile, seas_climYear)
         thresh_climYear[feb29 - 1] = 0.5 * thresh_climYear[feb29 - 2] + 0.5 * thresh_climYear[feb29]
@@ -144,10 +169,10 @@ def noaa_seas_thresh(climate_db_file,
         out_thresh[:, ilat, jlon] = thresh_climYear
 
         # Count
-        print('count={} of {}.'.format(counter, n_calc))
 
         # Cubes
         if (counter % 100000 == 0) or (counter == n_calc):
+            print('count={} of {}.'.format(counter, n_calc))
             print("Saving...")
             cubes = iris.cube.CubeList()
             time_coord = iris.coords.DimCoord(np.arange(lenClimYear), units='day', var_name='day')
@@ -214,6 +239,86 @@ def build_time_dict(t):
 
     return times
 
+def noaa_median_sst(outfile, climate_file=None, years = (1983, 2019), check=True):
+    """
+
+    Parameters
+    ----------
+    outfile
+    climate_file
+    years
+    check
+
+    Returns
+    -------
+
+    """
+    feb29 = 60
+    # Load
+    if climate_file is None:
+        climate_file = os.path.join(os.getenv('NOAA_OI'), 'NOAA_OI_climate_1983-2019.nc')
+    seasonalT = iris.load(climate_file, 'seasonalT')[0]
+    sT_data = seasonalT.data[:]
+
+    # Run it
+    sv_yr, sv_dy, sv_medSST, sv_medSSTa = [], [], [], []
+    for year in range(years[0], years[1] + 1):
+        print('year={}'.format(year))
+        # Load
+        noaa_file = os.path.join(os.getenv('NOAA_OI'), 'sst.day.mean.{}.nc'.format(year))
+        sst_cube = iris.load(noaa_file, 'sst')[0]
+        # Loop on days
+        SST = sst_cube.data[:]
+        for day in range(SST.shape[0]):
+            # print('day={}'.format(day))
+            SSTd = SST[day, :, :]
+            sv_yr.append(year)
+            sv_dy.append(day + 1)  # Jan 1 = 1
+            # Stats
+            sv_medSST.append(np.median(SSTd[~SSTd.mask]))
+            # import pdb; pdb.set_trace()
+            # Deal with leap year
+            offset = 0
+            if ((year - 1984) % 4) != 0 and (day >= feb29):
+                offset = 1
+            # SSTa
+            SSTa = SSTd - sT_data[day + offset, :, :]
+            sv_medSSTa.append(np.median(SSTa[~SSTd.mask]))
+
+    # Dates
+    tdates = [datetime.datetime(year, 1, 1) + datetime.timedelta(days=day - 1) for year, day in zip(sv_yr, sv_dy)]
+
+    # Pandas
+    pd_dict = dict(date=tdates, medSST=sv_medSST, medSSTa=sv_medSSTa)
+    pd_tbl = pandas.DataFrame(pd_dict)
+    pd_tbl = pd_tbl.set_index('date')
+
+    # Savgol
+    SSTa_filt = signal.savgol_filter(sv_medSSTa, 365, 3)
+    pd_tbl['medSSTa_savgol'] = SSTa_filt
+
+    # Check?
+    if check:
+        import matplotlib
+        from matplotlib import pyplot as plt
+        #
+        plt.clf()
+        ax = plt.gca()
+        #
+        dates = matplotlib.dates.date2num(pd_tbl.index)
+
+        ax.plot_date(dates, sv_medSSTa)
+        ax.plot_date(dates, SSTa_filt, 'r-')
+        # matplotlib.pyplot.plot_date(dates, values)
+        #
+        ax.set_ylabel('Median SSTa')
+        ax.set_xlabel('Year')
+        #
+        plt.show()
+        embed(header='312 of climate')
+
+    # Save pandas
+    pd_tbl.to_hdf(outfile, 'median_climate', mode='w')
 
 # Command line execution
 if __name__ == '__main__':
@@ -225,8 +330,18 @@ if __name__ == '__main__':
                          cut_sky=False)
 
     # Full Climate 1983-2019
-    if True:
+    if False:
         noaa_seas_thresh('/home/xavier/Projects/Oceanography/data/SST/NOAA-OI-SST-V2/NOAA_OI_climate_1983-2019.nc',
                          climatologyPeriod=(1983, 2019),
                          cut_sky=False)
 
+    # Median SSTa (savgol)
+    if False:
+        noaa_median_sst('../data/climate/noaa_median_climate_1983_2019.hdf', years=(1983,2019))
+
+    # Test
+    if True:
+        scale_file = os.path.join(resource_filename('mhw', 'data'), 'climate', 'noaa_median_climate_1983_2012.hdf')
+        noaa_seas_thresh('test_scaled.nc',
+                         climatologyPeriod=(1983, 1985),
+                         cut_sky=False, scale_file=scale_file)
